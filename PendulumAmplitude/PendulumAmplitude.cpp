eb275b11d10113dc735f5562d9b8d8271260a380
MkGodh <fenryrvla@yandex.ru>
2024-04-04 19:17:05 +0300

    Добавьте файлы проекта.

---
+++ PendulumAmplitude/PendulumAmplitude.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+    float StarterAmplitude;
+    float EndlessAmplitude;
+    int counter = 0;
+    cout << "Введите начальную амплитуду колебаний в сантиметрах: "; cin >> StarterAmplitude;
+    cout << "Введите конечную амплитуду колебаний в сантиметрах: "; cin >> EndlessAmplitude;
+
+    while (StarterAmplitude > EndlessAmplitude) {
+        float percentage = StarterAmplitude / 100 * 8.4;
+        StarterAmplitude -= percentage;
+        counter++;
+    }
+    cout << "Маятник качнется " << counter << " раз прежде чем остановится.";
+    return 0;
+}
